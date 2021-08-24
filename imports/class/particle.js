366564c4e09fbec1990e7f57ed834cd80bd29283
Paulo <cezarleigos28@outlook.com>
2021-08-24 15:54:31 -0300

    v0.1.7 -  Creating particles
    This time we started to define our functions to work with particles, we added some basic particles so that we can test their effects on screen in the future.

--- imports/class/particle.js
+++ imports/class/particle.js
@@ -3,5 +3,25 @@ class Particle {
 	constructor(particle, collision, life_time, animation){
 
 	}
+
+	function rain(speed, seed, layers){
+
+	}
+
+	function snow(speed, seed, layers){
+
+	}
+
+	function wind(speed, seed, layers){
+
+	}
+
+	function static_light(intensidade, layers){
+
+	}
+
+	function dynamic_light(intensidade,speed, layers){
+		
+	}
   
 }
